5d2f7204b0bfca285121b69e4224eef9c1028cae
Nylah Safforld <nylahsafforld@Nylahs-MacBook-Air.local>
2019-12-09 14:16:47 -0500

    Initial commit

---
+++ src/test/java/com/example/springboot401/Springboot401ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot401;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot401ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
